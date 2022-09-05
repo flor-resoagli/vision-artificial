f5e79baf338c1c3dee28366cc3851d7a70b58398
flor-resoagli <florencia.resoagli@ing.austral.edu.ar>
2022-09-05 16:35:44 -0300

    tp2 train

---
+++ tp2/tp2_test.py
@@ -0,0 +1,79 @@
+
+import cv2
+from joblib import load
+from math import copysign, log10
+
+from contour import get_contours, get_biggest_contour, compare_contours, get_contour_area
+from frame_editor import apply_color_convertion, adaptive_threshold, denoise, draw_contours
+from trackbar import create_trackbar, get_trackbar_value
+
+# carga el modelo
+clasificador = load('filename.joblib') 
+
+# etiquetaPredicha = clasificador.predict(invariantesDeHu)
+
+def get_hu_moments(contour):
+    moments = cv2.moments(contour)
+    hu_moments = cv2.HuMoments(moments)
+    for i in range(len(hu_moments)):
+        hu_moments[i] = -1 * copysign(1.0, hu_moments[i]) * log10(abs(hu_moments[i]))
+    return hu_moments
+
+def main():
+
+    window_name = 'Window'
+    cv2.namedWindow(window_name)
+    create_trackbar("BINARY", window_name, 151)
+    create_trackbar("DENOISE", window_name, 20)
+    create_trackbar("MATCH", window_name, 10)
+
+    cap = cv2.VideoCapture(0)
+    biggest_contour = None
+
+    while True:
+
+        ret, frame = cap.read()
+
+        binary_val = get_trackbar_value(trackbar_name="BINARY", window_name=window_name)
+        denoise_val = get_trackbar_value(trackbar_name="DENOISE", window_name=window_name)
+        match_val = get_trackbar_value(trackbar_name="MATCH", window_name=window_name)
+
+        gray_frame = apply_color_convertion(frame=frame, color=cv2.COLOR_RGB2GRAY)
+
+        ret2, adapt_frame = cv2.threshold(gray_frame, binary_val, 255, cv2.THRESH_BINARY)
+
+        frame_denoised = denoise(frame=adapt_frame, method=cv2.MORPH_ELLIPSE, radius=denoise_val)
+        contours = get_contours(frame=frame_denoised, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+
+        if len(contours) > 0:
+            for c in contours:
+                if get_contour_area(c)>8000 :
+                    
+                    draw_contours(frame=frame_denoised, contours=[c], color=(255, 255, 255), thickness=3)  #WHITE   
+
+                    # hu_moments = get_hu_moments(contour=c)
+
+                    
+
+                    moments = cv2.moments(c)
+
+            
+                    hu_moments = cv2.HuMoments(moments)
+
+                    print(hu_moments)
+
+
+                    predicted_tag = clasificador.predict(hu_moments)
+
+                    print(predicted_tag)
+
+                    # if predicted_tag == 0:
+
+                    #     cv2.drawContours(frame, [c],-1, (255, 0, 0), 2)
+                    #     x, y, w, h = cv2.boundingRect(c)
+                    #     text = 'Square'
+                    #     cv2.putText(frame, text, (x, y), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 255), 1, cv2.LINE_AA)
+
+
+
+main()
